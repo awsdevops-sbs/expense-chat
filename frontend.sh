e621f26524dcbd2d9a6f4e62ac4fb8a52f2bb5fd
akram <akram.khan200@gmail.com>
2025-02-16 20:34:48 +0530

    iteration chat1

--- frontend.sh
+++ frontend.sh
@@ -8,7 +8,7 @@ app_dir=/usr/share/nginx/html
 print "Installing Nginx..."
 install_package nginx
 
-print "Copying Nginx configuration..."
+print "Copying Nginx configuration....."
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 check_status $?
 
