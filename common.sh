56392e1fdf26f6d9cf30edab0791266c86e6daf5
akram <akram.khan200@gmail.com>
2025-02-17 00:02:51 +0530

    iteration chat12345

--- common.sh
+++ common.sh
@@ -51,7 +51,7 @@ app_req() {
   print "Extracting new application files...."
   #mkdir -p ${app_dir}  # Ensure directory exists
   cd ${app_dir} &>>$LOG
-  unzip /tmp/$component.zip  &>>$LOG
+  unzip /tmp/${component}.zip  &>>$LOG
   check_status $?
 
   print "Setting permissions for application directory...."
