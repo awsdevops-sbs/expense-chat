a29ce5d702d6ba9da911012606732eb597a34f32
akram <akram.khan200@gmail.com>
2025-02-17 00:58:48 +0530

    iteration chat12345j

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ install_package mysql-server  # Install MySQL server
 manage_service mysqld  # Start and enable MySQL service
 
 print "Checking MySQL root access..."
-echo 'SHOW DATABASES;' | mysql -h mysql-dev.awsdevops.sbs -uroot -p${mysql_root_password} &>>$LOG
+echo 'show databases' | mysql -h mysql-dev.awsdevops.sbs -uroot -p${mysql_root_password} &>>$LOG
 if [ $? -ne 0 ]; then
   print "Setting up MySQL root password..."
   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
